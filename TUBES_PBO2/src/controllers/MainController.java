4f3b997b4d9dba540f79c65894e41111571089ed
stephencandra <stephencandra1254@gmail.com>
2020-12-29 21:34:37 +0700

    monster.fxml

---
+++ TUBES_PBO2/src/controllers/MainController.java
@@ -0,0 +1,50 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
+public class MainController {
+    public GridPane mainMonster;
+    public Label lblHp1;
+    public ComboBox cmbMonster1;
+    public Label lblHp2;
+    public ComboBox cmbMonster2;
+    public Label lblHp3;
+    public ComboBox cmbMonster3;
+
+
+    public void comboAction1(ActionEvent actionEvent) {
+    }
+
+    public void cmbMonster1(ActionEvent actionEvent) {
+    }
+
+    public void fightAction1(ActionEvent actionEvent) {
+    }
+
+    public void comboAction2(ActionEvent actionEvent) {
+    }
+
+    public void cmbMonster2(ActionEvent actionEvent) {
+    }
+
+    public void fightAction2(ActionEvent actionEvent) {
+    }
+
+    public void addNewMonster(ActionEvent actionEvent) {
+    }
+
+    public void addElement(ActionEvent actionEvent) {
+    }
+
+    public void comboAction3(ActionEvent actionEvent) {
+    }
+
+    public void fightAction3(ActionEvent actionEvent) {
+    }
+
+    public void cmbMonster3(ActionEvent actionEvent) {
+    }
+}
